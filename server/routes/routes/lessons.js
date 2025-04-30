f483358be1a79d50a1319d60638d055367600abd
Salatiso <SPICEINC@GMAIL.COM>
2025-04-30 22:21:52 +0200

    Update lessons.js

--- server/routes/routes/lessons.js
+++ server/routes/routes/lessons.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
 
+// Placeholder for lesson management routes
+router.get('/', (req, res) => {
+    res.json({ message: 'Lesson management API' });
+});
+
+module.exports = router;
