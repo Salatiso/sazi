048a53ca90335f081852e602e3ededb3620ca366
Salatiso <SPICEINC@GMAIL.COM>
2025-04-30 22:16:57 +0200

    Update rateLimit.js

--- server/middleware/rateLimit.js
+++ server/middleware/rateLimit.js
@@ -1,1 +1,10 @@
+const rateLimit = require('express-rate-limit');
 
+const rateLimitMiddleware = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // Limit each IP to 5 requests per windowMs
+    message: 'Too many login attempts, please try again after 15 minutes.',
+    keyGenerator: (req) => req.body.email || req.body.username || req.ip
+});
+
+module.exports = { rateLimitMiddleware };
