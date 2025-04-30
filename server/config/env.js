120e85cddbfdb07808a8644e7ee4d39eee98f1e9
Salatiso <SPICEINC@GMAIL.COM>
2025-04-30 22:14:28 +0200

    Update env.js

--- server/config/env.js
+++ server/config/env.js
@@ -1,1 +1,5 @@
-
+module.exports = {
+    MONGODB_URI: 'mongodb+srv://Salatiso:<db_password>@cluster0.m9ua1va.mongodb.net/',
+    JWT_SECRET: 'your_jwt_secret_here', // Replace with a secure secret
+    PORT: 3000
+};
