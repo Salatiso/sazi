873d19f62a556636875ca43633f83874b81e4356
Salatiso <SPICEINC@GMAIL.COM>
2025-04-30 22:20:41 +0200

    Update classes.js

--- server/routes/routes/classes.js
+++ server/routes/routes/classes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
 
+// Placeholder for class management routes
+router.get('/', (req, res) => {
+    res.json({ message: 'Class management API' });
+});
+
+module.exports = router;
