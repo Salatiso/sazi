90b572c04d9b78924eeaed733ca59ebc7bed6b60
Salatiso <SPICEINC@GMAIL.COM>
2025-05-01 03:04:13 +0200

    Update Feedback.js

--- server/models/Feedback.js
+++ server/models/Feedback.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const FeedbackSchema = new mongoose.Schema({
+    contributionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Contribution', required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    type: { type: String, enum: ['like', 'report'], required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Feedback', FeedbackSchema);
