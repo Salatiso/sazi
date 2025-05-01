ed51cbcfd225b5a01a006e1ad698358b308332b0
Salatiso <SPICEINC@GMAIL.COM>
2025-05-01 03:01:42 +0200

    Update Class.js

--- server/models/Class.js
+++ server/models/Class.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
 
+const ClassSchema = new mongoose.Schema({
+    classCode: { type: String, required: true, unique: true },
+    type: { type: String, enum: ['structured', 'unstructured'], required: true },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    location: { type: String },
+    studentIds: [{
+        studentId: String,
+        progress: { type: Map, of: Number },
+        linkedParentEmail: String
+    }],
+    isPrivate: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Class', ClassSchema);
